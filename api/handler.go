d3a1b2b43f43b8c1803bf741c624ca06c7e2e1b2
m.kheyrati <mahmoud.kheyrati.fard@gmail.com>
2022-01-10 12:20:02 +0330

    fix pointer problem in http handlers

--- api/handler.go
+++ api/handler.go
@@ -23,14 +23,15 @@ func NewSchedulerHandlerImpl(config *config.SchedulerConfig) *SchedulerHandlerIm
 }
 
 func (s *SchedulerHandlerImpl) FirstComeFirstServe(ctx *fiber.Ctx) error {
-	var request *requests.ScheduleRequests
-	if err := ctx.BodyParser(request); err != nil {
+	var request requests.ScheduleRequests
+	if err := ctx.BodyParser(&request); err != nil {
 		ctx.JSON(fiber.Map{
 			"error": "invalid request format",
 		})
+
 		return nil
 	}
-	response, err := schedulers.ScheduleFirstComeFirstServe(request)
+	response, err := schedulers.ScheduleFirstComeFirstServe(&request)
 	if err != nil {
 		ctx.JSON(fiber.Map{"error": "can not proccess request"})
 		return nil
@@ -41,14 +42,14 @@ func (s *SchedulerHandlerImpl) FirstComeFirstServe(ctx *fiber.Ctx) error {
 }
 
 func (s *SchedulerHandlerImpl) RoundRobin(ctx *fiber.Ctx) error {
-	var request *requests.ScheduleRequests
-	if err := ctx.BodyParser(request); err != nil {
+	var request requests.ScheduleRequests
+	if err := ctx.BodyParser(&request); err != nil {
 		ctx.JSON(fiber.Map{
 			"error": "invalid request format",
 		})
 		return nil
 	}
-	response, err := schedulers.ScheduleRoundRobin(request)
+	response, err := schedulers.ScheduleRoundRobin(&request)
 	if err != nil {
 		ctx.JSON(fiber.Map{"error": "can not proccess request"})
 		return nil
@@ -59,14 +60,14 @@ func (s *SchedulerHandlerImpl) RoundRobin(ctx *fiber.Ctx) error {
 }
 
 func (s *SchedulerHandlerImpl) ShortestJobFirst(ctx *fiber.Ctx) error {
-	var request *requests.ScheduleRequests
-	if err := ctx.BodyParser(request); err != nil {
+	var request requests.ScheduleRequests
+	if err := ctx.BodyParser(&request); err != nil {
 		ctx.JSON(fiber.Map{
 			"error": "invalid request format",
 		})
 		return nil
 	}
-	response, err := schedulers.ScheduleShortestJobFirst(request)
+	response, err := schedulers.ScheduleShortestJobFirst(&request)
 	if err != nil {
 		ctx.JSON(fiber.Map{"error": "can not proccess request"})
 		return nil
@@ -77,14 +78,14 @@ func (s *SchedulerHandlerImpl) ShortestJobFirst(ctx *fiber.Ctx) error {
 }
 
 func (s *SchedulerHandlerImpl) MultilevelFeedbackQueue(ctx *fiber.Ctx) error {
-	var request *requests.ScheduleRequests
+	var request requests.ScheduleRequests
 	if err := ctx.BodyParser(request); err != nil {
 		ctx.JSON(fiber.Map{
 			"error": "invalid request format",
 		})
 		return nil
 	}
-	response, err := schedulers.ScheduleMultilevelFeedbackQueue(request)
+	response, err := schedulers.ScheduleMultilevelFeedbackQueue(&request)
 	if err != nil {
 		ctx.JSON(fiber.Map{"error": "can not proccess request"})
 		return nil
