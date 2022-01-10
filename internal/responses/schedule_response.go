95a4cc143e280e588ffb0d919ef15ba2ef37dbd4
m.kheyrati <mahmoud.kheyrati.fard@gmail.com>
2022-01-10 11:49:35 +0330

    add schedule response

---
+++ internal/responses/schedule_response.go
@@ -0,0 +1,18 @@
+package responses
+
+type ProcessResponse struct {
+	ProcessName    string  `json:"process_name"`
+	ResponseTime   float64 `json:"response_time"`
+	TurnAroundTime float64 `json:"turn_around_time"`
+	WaitingTime    float64 `json:"waiting_time"`
+}
+type ScheduleResponse struct {
+	TotalTime             float64           `json:"total_time"`
+	IdleTime              float64           `json:"idle_time"`
+	AverageWaitingTime    float64           `json:"average_waiting_time"`
+	AverageResponseTime   float64           `json:"average_response_time"`
+	AverageTurnAroundTime float64           `json:"average_turn_around_time"`
+	CpuUtilization        float64           `json:"cpu_utilization"`
+	CpuThroughput         float64           `json:"cpu_throughput"`
+	Details               []ProcessResponse `json:"details"`
+}
