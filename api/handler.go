7ba89343bf62f2599329beda7c966c4c8183d38a
m.kheyrati <mahmoud.kheyrati.fard@gmail.com>
2022-01-10 11:29:43 +0330

    register http handlers

--- api/handler.go
+++ api/handler.go
@@ -1,11 +1,41 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"os-project/config"
+)
 
 type SchedulerHandler interface {
-	FirstComeFirstServe(ctx *fiber.Ctx)
-	RoundRobin(ctx *fiber.Ctx)
-	ShortestJobFirst(ctx *fiber.Ctx)
-	MultilevelFeedbackQueue(ctx *fiber.Ctx)
-	AllAlgorithms(ctx *fiber.Ctx)
+	FirstComeFirstServe(ctx *fiber.Ctx) error
+	RoundRobin(ctx *fiber.Ctx) error
+	ShortestJobFirst(ctx *fiber.Ctx) error
+	MultilevelFeedbackQueue(ctx *fiber.Ctx) error
+	AllAlgorithms(ctx *fiber.Ctx) error
+}
+type SchedulerHandlerImpl struct {
+	config *config.SchedulerConfig
+}
+
+func NewSchedulerHandlerImpl(config *config.SchedulerConfig) *SchedulerHandlerImpl {
+	return &SchedulerHandlerImpl{config: config}
+}
+
+func (s *SchedulerHandlerImpl) FirstComeFirstServe(ctx *fiber.Ctx) error {
+	panic("implement me")
+}
+
+func (s *SchedulerHandlerImpl) RoundRobin(ctx *fiber.Ctx) error {
+	panic("implement me")
+}
+
+func (s *SchedulerHandlerImpl) ShortestJobFirst(ctx *fiber.Ctx) error {
+	panic("implement me")
+}
+
+func (s *SchedulerHandlerImpl) MultilevelFeedbackQueue(ctx *fiber.Ctx) error {
+	panic("implement me")
+}
+
+func (s *SchedulerHandlerImpl) AllAlgorithms(ctx *fiber.Ctx) error {
+	panic("implement me")
 }
--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+
 	"log"
+	api2 "os-project/api"
+	"os-project/config"
 )
 
 func main() {
+	schedulerConfig := config.GetSchedulerConfig()
+	schedulerHandler := api2.NewSchedulerHandlerImpl(schedulerConfig)
+
 	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(recover.New())
+
 	api := app.Group("/api")
 
 	v1 := api.Group("/v1")
 	{
-		v1.Get("/fcfs")
-		v1.Get("/rr")
-		v1.Get("sjf")
-		v1.Get("/mlfq")
-		v1.Get("/all")
+		v1.Get("/fcfs", schedulerHandler.FirstComeFirstServe)
+		v1.Get("/rr", schedulerHandler.RoundRobin)
+		v1.Get("sjf", schedulerHandler.ShortestJobFirst)
+		v1.Get("/mlfq", schedulerHandler.MultilevelFeedbackQueue)
+		v1.Get("/all", schedulerHandler.AllAlgorithms)
 	}
 
-
-
-	log.Fatalln(app.Listen(":9095"))
+	log.Fatalln(app.Listen(fmt.Sprintf(":%d", schedulerConfig.Port)))
 }
