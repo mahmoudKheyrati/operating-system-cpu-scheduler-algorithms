7738abef32357a7c988f548a6f250c0872641aaf
m.kheyrati <mahmoud.kheyrati.fard@gmail.com>
2022-01-10 12:08:11 +0330

    change http methods to post

--- main.go
+++ main.go
@@ -23,11 +23,11 @@ func main() {
 
 	v1 := api.Group("/v1")
 	{
-		v1.Get("/fcfs", schedulerHandler.FirstComeFirstServe)
-		v1.Get("/rr", schedulerHandler.RoundRobin)
-		v1.Get("sjf", schedulerHandler.ShortestJobFirst)
-		v1.Get("/mlfq", schedulerHandler.MultilevelFeedbackQueue)
-		v1.Get("/all", schedulerHandler.AllAlgorithms)
+		v1.Post("/fcfs", schedulerHandler.FirstComeFirstServe)
+		v1.Post("/rr", schedulerHandler.RoundRobin)
+		v1.Post("sjf", schedulerHandler.ShortestJobFirst)
+		v1.Post("/mlfq", schedulerHandler.MultilevelFeedbackQueue)
+		v1.Post("/all", schedulerHandler.AllAlgorithms)
 	}
 
 	log.Fatalln(app.Listen(fmt.Sprintf(":%d", schedulerConfig.Port)))
