38b41b01add2f9cec2aa8537b730fff1016d700e
m.kheyrati <mahmoud.kheyrati.fard@gmail.com>
2022-01-27 19:32:35 +0330

    implement sjf algorithm

--- internal/schedulers/shortest_job_first.go
+++ internal/schedulers/shortest_job_first.go
@@ -6,7 +6,6 @@ import (
 	"os-project/internal/core"
 	"os-project/internal/requests"
 	"os-project/internal/responses"
-	"os-project/internal/util"
 	"sort"
 	"sync"
 	"time"
@@ -18,159 +17,72 @@ type localProccess struct {
 }
 
 func ScheduleShortestJobFirst(request *requests.ScheduleRequests) (responses.ScheduleResponse, error) {
+	log.Println("running sjf algorithm ...")
+	// we have 3 level. 5, 8, fcfs
 	var ioDeviceCount = len(request.Jobs)
-
-	var wg sync.WaitGroup
-	wg.Add(cpuCoresCount + ioDeviceCount + proccessSchedulerGoroutineCount + completionProccessGoroutineCount)
-
-	// initialize processes
-	jobs := request.Jobs[:]
-	processes := make([]*core.Proccess, 0)
-	for i := 0; i < len(jobs); i++ {
-		processes = append(processes, &core.Proccess{
-			Job:           &jobs[i],
-			ScheduleTimes: make([]core.ScheduleTime, 0, 0),
-		})
-	}
-	printProcesses(processes)
 
 	// run cpu cores and io devices
 	var cpuWorkQueue = make(chan core.Proccess)
-	var ioWorkQueue = make(chan core.Proccess)
-	var ioDoneQueue = make(chan core.Proccess)
+	var ioWorkQueue = make(chan core.Proccess, len(request.Jobs))
+	var ioDoneQueue = make(chan core.Proccess, len(request.Jobs))
 	var completedProcesses = make(chan core.Proccess)
 
-	var contextSwitch = make(chan core.Proccess)
+	readyQueue := make([]core.Proccess, 0)
+	var readyQueueMutex sync.Mutex
+
+	scheduleNewProccess := func() {
+		time.Sleep(20 * time.Millisecond)
+		readyQueueMutex.Lock()
+		defer readyQueueMutex.Unlock()
+		// sort ready queue
+		readyQueue = sortShortestJob(readyQueue)
+		// pick shortest
+		if len(readyQueue) > 0 {
+			shortestJob := readyQueue[0]
+			readyQueue = readyQueue[1:]
+
+			select { // try to send the shortest job to cpu
+			case cpuWorkQueue <- shortestJob:
+			default:
+				readyQueue = append(readyQueue, shortestJob)
+			}
+		}
+	}
+
+	addProccessToReadyQueue := func(proccess core.Proccess) {
+		readyQueueMutex.Lock()
+		readyQueue = append(readyQueue, proccess)
+		readyQueueMutex.Unlock()
+
+		scheduleNewProccess()
+	}
+
+	contextSwitch := make(chan core.Proccess)
+	go func() {
+		for process := range contextSwitch {
+			log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ reading from context switch")
+
+			if process.Job.CpuTime1 == -1 && process.Job.IoTime != -1 { // we have io request
+				go scheduleNewProccess()
+				log.Println("pid:", process.Job.ProcessId, "send io request")
+				ioWorkQueue <- process
+			}
+
+			log.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ end of reading context switch")
+		}
+	}()
+
+	go func() { // ioDone
+		for proccess := range ioDoneQueue {
+			log.Println("pid:", proccess.Job.ProcessId, "ioDone!")
+			addProccessToReadyQueue(proccess)
+		}
+	}()
 
 	var cpuMetrics = make([]*core.CpuMetric, 0, 0)
 
-	// get completed proccess metrics
-	proccessDetails := make([]responses.ProcessResponse, 0)
-
-	go func(waitGroup *sync.WaitGroup) {
-		defer waitGroup.Done()
-		for process := range completedProcesses {
-			responseTime := process.ScheduleTimes[0].Execution.Sub(process.ScheduleTimes[0].Submission)
-			log.Println("pid:", process.Job.ProcessId, "proccess completed. schedule times: ", process.ScheduleTimes)
-			turnAroundTime := process.ScheduleTimes[len(process.ScheduleTimes)-1].Complete.Sub(process.ScheduleTimes[0].Submission)
-
-			var waitingTime float64 = 0
-			for _, s := range process.ScheduleTimes {
-				waitingTime += s.Execution.Sub(s.Submission).Seconds()
-			}
-
-			proccessDetails = append(proccessDetails, responses.ProcessResponse{
-				ProcessId:      process.Job.ProcessId,
-				ResponseTime:   responseTime.Seconds(),
-				TurnAroundTime: turnAroundTime.Seconds(),
-				WaitingTime:    waitingTime,
-			})
-
-			if len(proccessDetails) == len(request.Jobs) {
-				close(cpuWorkQueue)
-				close(ioWorkQueue)
-				close(ioDoneQueue)
-				close(contextSwitch)
-				break
-			}
-		}
-	}(&wg)
-
-	sortShortest := func() {
-		sort.SliceStable(processes, func(i, j int) bool {
-
-			if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 != -1 {
-				if processes[i].Job.ArrivalTime < processes[j].Job.ArrivalTime {
-					return processes[i].Job.ArrivalTime < processes[j].Job.ArrivalTime
-				}
-			}
-
-			if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 != -1 {
-				return processes[i].Job.CpuTime1 < processes[j].Job.CpuTime1
-			} else if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 == -1 {
-				return processes[i].Job.CpuTime1 < processes[j].Job.CpuTime2
-			} else if processes[i].Job.CpuTime1 == -1 && processes[j].Job.CpuTime1 != -1 {
-				return processes[i].Job.CpuTime2 < processes[j].Job.CpuTime1
-			}
-			return processes[i].Job.CpuTime2 < processes[j].Job.CpuTime2
-		})
-
-	}
-
-	var startTime time.Time
-	scheduleProccess := func(proccess *core.Proccess) {
-		log.Printf("proccess to schedule %+v with scheduleTime: %v", *proccess.Job, proccess.ScheduleTimes)
-		if len(proccess.ScheduleTimes) == 0 {
-			proccess.ScheduleTimes = append(proccess.ScheduleTimes, core.ScheduleTime{
-				Submission: time.Now().Add(time.Duration(proccess.Job.ArrivalTime) * time.Second),
-			})
-			log.Println("pid:", proccess.Job.ProcessId, "RUUUUNN for first time.", (startTime.Add(time.Duration(proccess.Job.ArrivalTime) * time.Second).Sub(time.Now())))
-			if startTime.Add(time.Duration(proccess.Job.ArrivalTime)*time.Second).Sub(time.Now()) < 0 {
-				log.Println("pid:", proccess.Job.ProcessId, "schedule for first time immediately.")
-				log.Println("pid:", proccess.Job.ProcessId, "sending process to cpuWorkQueue immediately")
-				cpuWorkQueue <- *proccess
-				return
-			}
-			select {
-			case <-time.After(startTime.Add(time.Duration(proccess.Job.ArrivalTime) * time.Second).Sub(time.Now())):
-				log.Println("pid:", proccess.Job.ProcessId, "schedule for first time.")
-				log.Println("pid:", proccess.Job.ProcessId, "sending process to cpuWorkQueue")
-				cpuWorkQueue <- *proccess
-			}
-		} else {
-			log.Println("pid:", proccess.Job.ProcessId, "schedule for " , len(proccess.ScheduleTimes), "times")
-			log.Println("pid:", proccess.Job.ProcessId, "schedule times " , proccess.ScheduleTimes)
-			log.Printf("%+v", proccess)
-
-			proccess.ScheduleTimes[len(proccess.ScheduleTimes)-1].Complete = time.Now()
-
-			log.Println("pid:", proccess.Job.ProcessId, "sending process to cpuWorkQueue")
-			cpuWorkQueue <- *proccess
-		}
-	}
-
-	var reSchedule = func() {
-		if len(processes) > 0 {
-			sortShortest()
-			log.Println("pid:", processes[0].Job.ProcessId, "******************************************** select as shortest process to schedule.")
-			scheduleProccess(processes[0])
-			log.Printf("-------------------- removing %+v %+v ", processes[0].Job, processes[0].ScheduleTimes)
-			processes = processes[1:]
-			printProcesses(processes)
-		} else {
-			log.Println("((((((((((((((((((((((((((((((((((( processes are empty")
-		}
-	}
-
-	go func() {
-		v, ok := <-contextSwitch
-		if !ok {
-			log.Println("OOOOOOOOOOOOOOOOOOOOOOOOO context closed")
-			return
-		}
-		// issue io-request
-		go func() {
-			ioWorkQueue <- v
-		}()
-
-		// reschedule
-		log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ context switch issue reschedule")
-		reSchedule()
-	}()
-
-	go func() {
-		v, ok := <-ioDoneQueue
-		if !ok {
-			log.Println("OOOOOOOOOOOOOOOOOOOOOOOOO ioDoneQueue closed")
-			return
-		}
-
-		log.Println("pid:", v.Job.ProcessId, "******************************** receive proccess in reschedule proccess")
-		processes = append(processes, &v)
-		log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ioDone issue reschedule")
-		printProcesses(processes)
-		reSchedule()
-	}()
+	var wg sync.WaitGroup
+	wg.Add(cpuCoresCount + ioDeviceCount + proccessSchedulerGoroutineCount + completionProccessGoroutineCount)
 
 	// by this technique we could have multi-core and multi-io device at once
 	for i := 0; i < cpuCoresCount; i++ {
@@ -186,33 +98,82 @@ func ScheduleShortestJobFirst(request *requests.ScheduleRequests) (responses.Sch
 	go func(waitGroup *sync.WaitGroup) {
 		defer waitGroup.Done()
 
-		log.Println("Schedule first process.")
-		printProcesses(processes)
-		startTime=time.Now()
-		reSchedule()
-		printProcesses(processes)
+		// sort jobs by arrival time
+		jobs := request.Jobs[:]
 
+		// schedule jobs
+		for _, job := range jobs {
+			go func(job requests.Job) {
+				proccess := core.Proccess{
+					Job:           &job,
+					ScheduleTimes: make([]core.ScheduleTime, 0, 0),
+				}
+
+				select {
+				case <-time.After(time.Now().Add(time.Duration(proccess.Job.ArrivalTime) * time.Second).Sub(time.Now())):
+					addNewScheduleTimeToProccess(&proccess)
+					log.Println("pid:", proccess.Job.ProcessId, "send process to readyQueue")
+					addProccessToReadyQueue(proccess)
+				}
+			}(job)
+		}
+	}(&wg)
+
+	go func() { // schedule multilevel feedback queue.
+		//for {
+		//	cpuWorkQueue <- proccess
+		//}
+	}()
+
+	// get completed proccess metrics
+	proccessDetails := make([]responses.ProcessResponse, 0)
+
+	go func(waitGroup *sync.WaitGroup) {
+		defer waitGroup.Done()
+		for process := range completedProcesses {
+			details := generateProcessDetails(process)
+			proccessDetails = append(proccessDetails, details)
+
+			if len(proccessDetails) == len(request.Jobs) {
+				close(cpuWorkQueue)
+				close(ioWorkQueue)
+				break
+			}
+		}
 	}(&wg)
 
 	wg.Wait()
 
-	averageWaitingTime, averageResponseTime, averageTimeAroundTime := util.CalculateAverage(proccessDetails)
-
-	utilization := 1 - (cpuMetrics[0].IdleTime.Seconds() / cpuMetrics[0].TotalTime.Seconds())
-	var jobCount = len(request.Jobs)
-	var response = responses.ScheduleResponse{
-		TotalTime:             cpuMetrics[0].TotalTime.Seconds(),
-		IdleTime:              cpuMetrics[0].IdleTime.Seconds(),
-		CpuUtilization:        utilization,
-		CpuThroughput:         float64(jobCount) / cpuMetrics[0].TotalTime.Seconds(),
-		AverageWaitingTime:    averageWaitingTime,
-		AverageResponseTime:   averageResponseTime,
-		AverageTurnAroundTime: averageTimeAroundTime,
-		Details:               proccessDetails,
-	}
-
+	response := generateResponse(len(request.Jobs), proccessDetails, cpuMetrics)
 	log.Printf("response is: %+v", response)
 	return response, nil
+
+}
+
+func sortShortestJob(processes []core.Proccess) []core.Proccess {
+	sort.SliceStable(processes, func(i, j int) bool {
+
+		//if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 != -1 {
+		//	if processes[i].Job.ArrivalTime < processes[j].Job.ArrivalTime {
+		//		return processes[i].Job.ArrivalTime < processes[j].Job.ArrivalTime
+		//	}
+		//}
+
+		if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 != -1 {
+			return processes[i].Job.CpuTime1 < processes[j].Job.CpuTime1
+		} else if processes[i].Job.CpuTime1 != -1 && processes[j].Job.CpuTime1 == -1 {
+			return processes[i].Job.CpuTime1 < processes[j].Job.CpuTime2
+		} else if processes[i].Job.CpuTime1 == -1 && processes[j].Job.CpuTime1 != -1 {
+			return processes[i].Job.CpuTime2 < processes[j].Job.CpuTime1
+		}
+		return processes[i].Job.CpuTime2 < processes[j].Job.CpuTime2
+	})
+	fmt.Printf("sorted proccesses: ")
+	for i := 0; i < len(processes); i++ {
+		fmt.Printf("job: %+v, ", processes[i].Job)
+	}
+	fmt.Println()
+	return processes
 }
 
 func printProcesses(processes []*core.Proccess) {
